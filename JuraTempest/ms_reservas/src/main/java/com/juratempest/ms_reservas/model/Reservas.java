b5c0e8e58e7435bb4e94af46ba02c98676f7ddde
lukms19 <luk.mezas@duocuc.cl>
2026-05-07 12:35:33 -0400

    model

--- JuraTempest/ms_reservas/src/main/java/com/juratempest/ms_reservas/model/Reservas.java
+++ JuraTempest/ms_reservas/src/main/java/com/juratempest/ms_reservas/model/Reservas.java
@@ -1,25 +1,6 @@
 package com.juratempest.ms_reservas.model;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "reservas",
-            uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"maquina", "bloque_horario"})
-            }
-    )
-
-
 public class Reservas {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private Long usuarioId;
-    private Long maquinaId;
-    private Long bloqueId;
-    private LocalDateTime fechaReserva;
-    private String estado;
+    
     
 }

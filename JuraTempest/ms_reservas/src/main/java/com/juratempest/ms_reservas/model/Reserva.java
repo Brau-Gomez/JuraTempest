dab6c8e9a83f2a7b45a0b51d8042e7ddc0e28ca4
lukms19 <luk.mezas@duocuc.cl>
2026-05-10 01:13:53 -0400

    agregando model

--- JuraTempest/ms_reservas/src/main/java/com/juratempest/ms_reservas/model/Reserva.java
+++ JuraTempest/ms_reservas/src/main/java/com/juratempest/ms_reservas/model/Reserva.java
@@ -1,3 +1,40 @@
+package com.juratempest.ms_reservas.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reservas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private Long usuarioId;
+
+    @Column(nullable = false)
+    private Long maquinaId;
+
+    @Column(nullable = false)
+    private Long bloqueId;
+
+    @Column(nullable = false)
+    private LocalDate fechaReserva;
+
+    @Column(nullable = false)
+    private String estado;
 
 }

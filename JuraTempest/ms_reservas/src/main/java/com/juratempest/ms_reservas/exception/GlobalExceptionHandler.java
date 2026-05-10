c5ff111cb1cbaba597591af6add3a25246946523
lukms19 <luk.mezas@duocuc.cl>
2026-05-10 01:52:49 -0400

    agregando exception

---
+++ JuraTempest/ms_reservas/src/main/java/com/juratempest/ms_reservas/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.juratempest.ms_reservas.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String,Object>> manejarNotFound(ResourceNotFoundException ex){
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(error(HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> manejarValidacion(MethodArgumentNotValidException ex){
+        Map<String, String> validaciones = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+            .forEach(error -> 
+                validaciones.put(error.getField(), error.getDefaultMessage())
+            );
+        return ResponseEntity.badRequest()
+            .body(error(HttpStatus.BAD_REQUEST,"Datos invalidos"));
+    }
+
+    private Map<String, Object > error(HttpStatus status, String mensaje){
+        Map<String,Object> body = new HashMap<>();
+        body.put("timestap",LocalDateTime.now());
+        body.put("status",status.value());
+        body.put("error",status.getReasonPhrase());
+        body.put("mensaje",mensaje);
+        return body;
+    }
+
+    
+}
---
+++ JuraTempest/ms_reservas/src/main/java/com/juratempest/ms_reservas/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.juratempest.ms_reservas.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String mensaje){
+        super(mensaje);
+    }
+}

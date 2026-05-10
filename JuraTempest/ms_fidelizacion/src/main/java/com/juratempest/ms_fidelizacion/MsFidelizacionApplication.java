8de0f5cd6dee23f9839695a8b2bbcb55711547ff
lukms19 <luk.mezas@duocuc.cl>
2026-05-10 19:04:42 -0400

    model fidelizacion

---
+++ JuraTempest/ms_fidelizacion/src/main/java/com/juratempest/ms_fidelizacion/MsFidelizacionApplication.java
@@ -0,0 +1,13 @@
+package com.juratempest.ms_fidelizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFidelizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFidelizacionApplication.class, args);
+	}
+
+}
---
+++ JuraTempest/ms_fidelizacion/src/test/java/com/juratempest/ms_fidelizacion/MsFidelizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juratempest.ms_fidelizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFidelizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

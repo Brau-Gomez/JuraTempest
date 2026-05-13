67cd9d89e4fec0222c7b3b86b7912fdc7fe6dec6
Brau-Gomez <another.48@icloud.com>
2026-05-12 21:08:41 -0400

    Se borra linea sin ocupar en GlobalException. Se agrega archivo SQL para carga inicial de datos con LiquiBase.

---
+++ JuraTempest/ms_reservas/src/main/resources/db/changelog/db.changelog.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+CREATE TABLE reservas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id BIGINT NOT NULL,
+    maquina_id BIGINT NOT NULL,
+    horario_id BIGINT NOT NULL,
+    fecha_reserva DATE NOT NULL,
+    estado VARCHAR(30) NOT NULL
+);
+
+--changeset juratempest:reservas-2
+INSERT INTO reservas (usuario_id, maquina_id, horario_id, fecha_reserva, estado) VALUES
+(3, 1, 1, '2026-05-07', 'ACTIVA'),
+(4, 2, 2, '2026-05-07', 'ACTIVA'),
+(5, 3, 3, '2026-05-07', 'ACTIVA'),
+(6, 4, 4, '2026-05-07', 'CANCELADA'),
+(7, 5, 5, '2026-05-07', 'ACTIVA'),
+(8, 7, 6, '2026-05-07', 'FINALIZADA'),
+(9, 8, 7, '2026-05-08', 'ACTIVA'),
+(10, 1, 8, '2026-05-08', 'CANCELADA'),
+(3, 2, 9, '2026-05-08', 'FINALIZADA'),
+(4, 3, 10, '2026-05-08', 'ACTIVA');

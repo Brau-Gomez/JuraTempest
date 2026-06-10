1a82c36f5589c69c4583d9e34b548b24494ba110
Brau-Gomez <brau.gomez@duocuc.cl>
2026-06-10 14:11:36 -0400

    fix: Se agrega codigo en clases Notificacion y NotificacionDTO.

--- JuraTempest/ms_notificaciones/src/main/java/com/juratempest/ms_notificaciones/exception/BadRequestException.java
+++ JuraTempest/ms_notificaciones/src/main/java/com/juratempest/ms_notificaciones/exception/BadRequestException.java
@@ -3,7 +3,7 @@ package com.juratempest.ms_notificaciones.exception;
 public class BadRequestException extends RuntimeException {
 
     public BadRequestException(String message) {
-        // TODO: Llamar al constructor padre RuntimeException con el mensaje recibido.
+        
         super(message);
     }
 }

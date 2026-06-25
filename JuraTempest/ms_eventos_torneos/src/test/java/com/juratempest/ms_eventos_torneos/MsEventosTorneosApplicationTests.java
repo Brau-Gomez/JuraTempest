f013be00ce033047d05d84b0e7984ca926e7de87
Brau-gomez <another.48@icloud.com>
2026-06-25 04:57:59 -0400

    Optimizar la configuración de Docker Compose incorporando comprobaciones de salud (health checks) para los servicios y actualizar las dependencias de los microservicios; mejorar la documentación del README para aportar mayor claridad, requisitos de entorno e instrucciones detalladas de despliegue.

--- JuraTempest/ms_eventos_torneos/src/test/java/com/juratempest/ms_eventos_torneos/MsEventosTorneosApplicationTests.java
+++ JuraTempest/ms_eventos_torneos/src/test/java/com/juratempest/ms_eventos_torneos/MsEventosTorneosApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsNotificacionesApplicationTests {
+class MsEventosTorneosApplicationTests {
 
     @Test
     void contextLoads() {
